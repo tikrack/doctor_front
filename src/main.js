44168411faa74b2593c0c8eb9b34dec6739cabff
mohamadreza <mohamadreza1388.org@gmail.com>
2025-02-07 16:05:04 +0330

    feat(toast): try to cotum toast

--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  Toastify({
-    text: "This is a toast",
-    duration: 3000,
-  }).showToast();
+  //Element
+  const $ = document;
+
+  //Elements
+  const elm_call = $.querySelector("#call");
+
+  //Variable
+
+  //Functions
+
+  //Events
+  window.addEventListener("load", () => {
+    console.log("ok");
+  });
+
+  elm_call.addEventListener("click", (e) => {
+    Toastify({
+      text: "This is a toast",
+      duration: 30000,
+      gravity: "bottom",
+      position: "center",
+    }).showToast();
+  });
 });
